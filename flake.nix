e97622f2c744c8835867efe9c0ab661108d75c9f
NaturalSpottingSmite <239067163+NaturalSpottingSmite@users.noreply.github.com>
2026-02-01 06:34:31 +0000

    Fix overlay

--- flake.nix
+++ flake.nix
@@ -3,14 +3,21 @@
 
   outputs = { nixpkgs, self, ... }: let
     forEachSystem = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (s: f nixpkgs.legacyPackages.${s});
+    pythonOverlay = final: prev: {
+      asyncwinrm = final.callPackage ./. { };
+    };
   in {
     overlays = {
       default = self.overlays.asyncwinrm;
       asyncwinrm = final: prev: {
+        python313 = prev.python313.override {
+          packageOverrides = pythonOverlay;
+        };
+        python314 = prev.python314.override {
+          packageOverrides = pythonOverlay;
+        };
         python3 = prev.python3.override {
-          packageOverrides = final: prev: {
-            asyncwinrm = final.callPackage ./. { };
-          };
+          packageOverrides = pythonOverlay;
         };
       };
     };
